8ba7894c4c2d64afc40d3529634a1b6a32f04c72
non4214 <87809144+non4214@users.noreply.github.com>
2023-02-13 23:20:56 +0700

    First commit

---
+++ googlefrom.py
@@ -0,0 +1,26 @@
+
+from selenium import webdriver
+import time
+import sys
+
+driver = webdriver.Chrome("C:/Users/ADMIN/Desktop/google/chromedriver.exe")
+url = "https://docs.google.com/forms/d/e/1FAIpQLSfrlCBLWyl05xQPgihq69qKU3HqmLc5C2InPMwDXsFYBktTLA/viewform?usp=sf_link"
+
+driver.get(url)
+
+def fill_from(firstname, lastname ,gemder ,age):
+    inputs = driver.find_elements_by_class_name("quantumWizTextinputInput")
+    radiobuttons = driver.find_elements_by_class_name("appsMaterialWizToggleFladiogroupEl")
+
+    time.sleep(10)
+
+    inputs_array = [
+        firstname, lastname,age
+        ]
+
+    for i in range(len(inputs)):
+            inputs[i].clear()
+            inputs[i].send_keys(inputs_array[i])
+
+fill_from("ณัฐชนน","วะลับ","ชาย","22")
+
